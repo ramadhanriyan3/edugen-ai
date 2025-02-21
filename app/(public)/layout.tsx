a2f189070c68d89dcec9a74449a3bac97e02ba65
Riyan Ramadhan <ramadhanriyan3@gmail.com>
2025-02-22 00:02:38 +0700

    feat: add Landing Page

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,93 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { LuInstagram, LuFacebook, LuTwitter, LuMail } from "react-icons/lu";
+
+const PublicLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="w-full flex flex-col min-h-screen p-4">
+      <nav className="flex justify-between items-center">
+        <div className="flex items-end gap-x-2 w-fit">
+          <Image
+            alt="logo"
+            src={"/eduGen-Logo.png"}
+            width={50}
+            height={50}
+            className=" w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10"
+          />
+          <h1 className="font-bold leading-none text-base  sm:text-lg text-primary text-center">
+            Edugen AI
+          </h1>
+        </div>
+        <div className="w-fit flex gap-x-2 md:gap-x-4 items-center font-light text-xs sm:text-sm md:text-base ">
+          <Link href={"/news"} className="text-accent-foreground">
+            News
+          </Link>
+          <Link href={"/tutorial"} className="text-accent-foreground">
+            Tutorial
+          </Link>
+          <Link href={"/suport"} className="text-accent-foreground">
+            Support
+          </Link>
+          <Link
+            href={"/log-in"}
+            className=" font-semibold border border-primary text-primary py-1 px-2 rounded-sm"
+          >
+            Log in
+          </Link>
+        </div>
+      </nav>
+      <div className="flex-1 bg-transparent items-center justify-center flex">
+        {children}
+      </div>
+      <footer className="py-5 md:pb-8 flex gap-y-5 justify-between flex-wrap-reverse">
+        <div className="flex w-fit flex-col gap-y-4 mx-auto sm:mx-0">
+          <div className="flex items-end gap-x-2 w-fit">
+            <Image
+              alt="logo"
+              src={"/eduGen-Logo.png"}
+              width={50}
+              height={50}
+              className=" w-5 h-5 md:w-10 md:h-10"
+            />
+            <h1 className="font-bold leading-none text-sm font- md:text-medium text-primary text-center">
+              Edugen AI
+            </h1>
+          </div>
+          <div className="flex space-x-2 ">
+            <Link href={"#"}>
+              <LuMail className="w-3 h-3 md:w-5 md:h-5 text-primary" />
+            </Link>
+            <Link href={"#"}>
+              <LuFacebook className="w-3 h-3 md:w-5 md:h-5 text-primary" />
+            </Link>
+            <Link href={"#"}>
+              <LuInstagram className="w-3 h-3 md:w-5 md:h-5 text-primary" />
+            </Link>
+            <Link href={"#"}>
+              <LuTwitter className="w-3 h-3 md:w-5 md:h-5 text-primary" />
+            </Link>
+          </div>
+          <p className="text-[10px] md:text-xs text-accent-foreground">
+            &copy; 2025 Edugen AI. All right reserved.
+          </p>
+        </div>
+        <div className="flex mx-auto sm:mx-0 w-fit gap-x-4 lg:gap-8 text-accent-foreground/90">
+          <div className="flex-col flex gap-x-2 text-[10px] md:text-sm">
+            <p className="font-semibold">Support</p>
+            <Link href={"#"}>Community</Link>
+            <Link href={"#"}>Issues Report</Link>
+          </div>
+          <div className="flex-col flex gap-x-2 text-[10px] md:text-sm">
+            <p className="font-semibold">Legal & Safety</p>
+            <Link href={"#"}>Privacy Policy</Link>
+            <Link href={"#"}>Term of Use</Link>
+            <Link href={"#"}>Report Vulnerabilities</Link>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default PublicLayout;
---
+++ components/fadeInWrapper.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
+
+interface FadeInWrapperProps {
+  children: ReactNode;
+  variant: "top" | "bottom" | "left" | "right";
+  delay?: number;
+}
+
+const FadeInWrapper = ({
+  children,
+  variant,
+  delay = 0,
+}: FadeInWrapperProps) => {
+  const [isVisible, setIsvisible] = useState(false);
+
+  let fadeIn;
+
+  switch (variant) {
+    case "top":
+      fadeIn = isVisible
+        ? "opacity-100 translate-y-0"
+        : "opacity-0 -translate-y-2";
+      break;
+    case "bottom":
+      fadeIn = isVisible
+        ? "opacity-100 translate-y-0"
+        : "opacity-0 translate-y-2";
+      break;
+    case "left":
+      fadeIn = isVisible
+        ? "opacity-100 translate-x-0"
+        : "opacity-0 -translate-x-2";
+      break;
+    case "right":
+      fadeIn = isVisible
+        ? "opacity-100 translate-x-0"
+        : "opacity-0 translate-x-2";
+      break;
+    default:
+      fadeIn = "opacity-100 translate-y-0";
+  }
+
+  useEffect(() => {
+    setIsvisible(true);
+  }, []);
+
+  return (
+    <div
+      className={`transition-all duration-1000 ease-in ${fadeIn}`}
+      style={{ transitionDelay: `${delay}ms` }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FadeInWrapper;
