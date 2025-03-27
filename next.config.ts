57b1aec3b3054faf4923b1663d812b4ccf51f5fc
Riyan Ramadhan <91193226+ramadhanriyan3@users.noreply.github.com>
2025-03-28 04:48:44 +0700

    Development (#6)
    * fix: update next-config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*.vercel.app"],
+    },
+  },
 };
 
 export default nextConfig;
