d5254da9fe680cc64622bfbc7409496a7b75eeba
Riyan Ramadhan <91193226+ramadhanriyan3@users.noreply.github.com>
2025-06-06 15:20:58 +0700

    Development (#12)
    * feat: add Landing Page
    
    * feat: add sidebar
    
    * feat: add oauth and addjustment style
    
    * feat: add server action, delete alert dialog, adjustment style
    
    * fix: move action folder
    
    * fix: sidebar item rendering

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -77,6 +77,23 @@ const SignInPage = () => {
             >
               Sign In
             </Button>
+            <div className="flex items-center">
+              <hr className="flex-grow border-t border-primary" />
+              <span className="px-2 text-sm text-primary">OR</span>
+              <hr className="flex-grow border-t border-primary" />
+            </div>
+            <Button
+              variant="outline"
+              className="w-full border-primary text-primary flex items-center hover:text-primary-foreground"
+            >
+              <Image
+                alt="google-icon"
+                src={"/google-icon.svg"}
+                width={20}
+                height={20}
+              />{" "}
+              Sign in with Google
+            </Button>
           </form>
         </Form>
         <div className="flex items-center w-full">
